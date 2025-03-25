5e7ac7fe55ffe73a7cc4a79c1c89f4e0b2b21c48
SMCorey <sloanc97@gmail.com>
2025-03-25 12:29:25 -0300

    Add project files.

---
+++ Controllers/TicketOrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tickethub_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketOrderController : ControllerBase
+    {
+        private readonly ILogger<TicketOrderController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TicketOrderController(ILogger<TicketOrderController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+
+        // GET: api/<TicketOrderController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TicketOrderController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TicketOrderController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        //// PUT api/<TicketOrderController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<TicketOrderController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Data/TicketOrder.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tickethub_api.Data
+{
+    public class TicketOrder
+    {
+        public int ConcertId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [CreditCard]
+        public string CreditCard { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/([0-9]{2})$", ErrorMessage = "Expiration date must be in MM/YY format")]
+        public string Expiration { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3,4}$", ErrorMessage = "Security code must be 3 or 4 digits")]
+        public string SecurityCode { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+    }
+}
