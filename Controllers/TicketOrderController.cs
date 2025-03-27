9c78c6b15a8faebdacd5a753b0065295fe6078f6
SMCorey <sloanc97@gmail.com>
2025-03-27 16:19:02 -0300

    added azure queue and updated post functionality.

--- Controllers/TicketOrderController.cs
+++ Controllers/TicketOrderController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json;
+using tickethub_api.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,44 +14,76 @@ namespace tickethub_api.Controllers
     {
         private readonly ILogger<TicketOrderController> _logger;
         private readonly IConfiguration _configuration;
+        QueueClient queueClient;
+        string queueName = "tickethub";
+        string connectionString;
 
         public TicketOrderController(ILogger<TicketOrderController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("AzureStorage");
+            queueClient = new QueueClient(connectionString, queueName);
+            queueClient.CreateIfNotExistsAsync();
         }
 
 
-        // GET: api/<TicketOrderController>
+
+        // Examples:
+        // serialize an object to json
+        //string message = JsonSerializer.Serialize(someObject);
+
+        // send string message to queue
+        //await queueClient.SendMessageAsync(message);
+
+
+
+        //GET: api/<TicketOrderController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<TicketOrderController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //GET api/<TicketOrderController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
         // POST api/<TicketOrderController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] TicketOrder ticketOrder)
         {
-        }
+            try
+            {
+                string messageJson = JsonSerializer.Serialize(ticketOrder);
 
-        //// PUT api/<TicketOrderController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
+                await queueClient.SendMessageAsync(messageJson);
 
-        // DELETE api/<TicketOrderController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                return Ok("Order has been placed");
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+
+
+            }
+
+            //// PUT api/<TicketOrderController>/5
+            //[HttpPut("{id}")]
+            //public void Put(int id, [FromBody] string value)
+            //{
+            //}
+
+            // DELETE api/<TicketOrderController>/5
+            //[HttpDelete("{id}")]
+            //public void Delete(int id)
+            //{
+            //}
         }
     }
 }
